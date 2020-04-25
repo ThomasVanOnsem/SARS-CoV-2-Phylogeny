ee8e6118955f3a136f8832112e42a7c02b8bef8b
miguel <Miguel.Dagrain@student.uantwerpen.be>
2020-04-25 22:40:07 +0200

    an add page base on the current format in fasta files

---
+++ www/static/js/responsiveness_add.js
@@ -0,0 +1,23 @@
+function triggerToggle(event){
+	event.preventDefault();
+	var options = $('#type-sequence-input').children();
+	options.each(function () {
+		$(this).blur();
+		if ($(this).hasClass('is-selected')) {
+			$(this).removeClass('is-selected');
+			$(this).removeClass('is-primary');
+		} else {
+			$(this).addClass('is-selected');
+			$(this).addClass('is-primary');
+
+			//now we change the page
+			if($(this).attr('id') === 'choose-nuc'){
+				$('#nucleotide-adding').attr('style', '');
+				$('#amino-acid-adding').attr('style', 'display:none;');
+			} else {
+				$('#nucleotide-adding').attr('style', 'display:none;');
+				$('#amino-acid-adding').attr('style', '');
+			}
+		}
+	})
+}
