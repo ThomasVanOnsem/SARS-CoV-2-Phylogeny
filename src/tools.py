572b1f4033860171528f2598387c9f57d0c48f63
Max Van Houcke <max.vanhoucke@student.uantwerpen.be>
2020-05-12 11:46:24 +0200

    Easier data location

--- src/tools.py
+++ src/tools.py
@@ -1,6 +1,8 @@
-from config import config
 from os import path
+
+THIS_FOLDER = path.dirname(path.abspath(__file__))
+DATA_FOLDER = path.join(THIS_FOLDER, '../data')
 
 
 def getDataLocation(locationInDataDirectory):
-    return path.join(config['data-directory'], locationInDataDirectory)
+    return path.join(DATA_FOLDER, locationInDataDirectory)
